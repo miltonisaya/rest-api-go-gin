7d6572748ec603df7ed0778b4a342689a86f63c2
Milton Isaya <miltonisaiah@gmail.com>
2026-06-12 16:28:59 +0300

    Initialise application struct with DB models, port, and JWT secret

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"database/sql"
+	"log"
+	"rest-api-in-gin/cmd/internal/database"
+	"rest-api-in-gin/cmd/internal/env"
+
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type application struct {
+	port      int
+	jwtSecret string
+	models    *database.Models
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port:      env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-key"),
+		models:    models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
+}
