0385f4fa9ef5021601c93116ffad5665b69c5d55
Milton Isaya <miltonisaiah@gmail.com>
2026-06-12 16:52:23 +0300

    Register v1 event routes on the Gin router

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) routes() http.Handler {
+	g := gin.Default()
+
+	v1 := g.Group("/v1")
+	{
+		v1.POST("/events", app.createEvent)
+		v1.GET("/events", app.getAllEvents)
+		v1.GET("/events/:id", app.getEvent)
+		v1.PUT("/events/:id", app.createEvent)
+		v1.DELETE("/events/:id", app.deleteEvent)
+	}
+
+	return g
+}
